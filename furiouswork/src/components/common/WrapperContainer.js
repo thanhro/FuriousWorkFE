0a5b96536f7be13316915fdc6f9279cebc40f3b0
thanhro <lethanh309@gmail.com>
2020-07-07 00:09:33 +0700

    init container

---
+++ furiouswork/src/components/common/WrapperContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ClassNames from 'classnames'
+import Styles from './WrapperContainer.module.scss'
+import Grid from '@material-ui/core/Grid'
+
+const  WrapperContainer = () => {
+
+    return (
+
+    )
+}
+
+WrapperContainer.propTypes = {
+
+}
+
+export default WrapperContainer
